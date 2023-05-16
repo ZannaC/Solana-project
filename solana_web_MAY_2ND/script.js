8c7dc984c48f8e65fe9b3df229054a67ab2ff680
ZannaC <zhanna_s1@inbox.lv>
2023-05-16 21:00:56 +0300

    Add methods to repeat blocks

--- solana_web_MAY_2ND/script.js
+++ solana_web_MAY_2ND/script.js
@@ -1,29 +1,73 @@
 "use strict";
 // Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
-  // Get the parent container element
-  var container = document.querySelector(".dig_deeper_container");
+  var container = document.getElementById("container");
+  var infoData = [
+    {
+      theme: "Solana Cookbook",
+      info: "The Solana Cookbook is a developer resource that provides the essential concepts and references for building applications on Solana.",
+    },
+    {
+      theme: "Solana Stack Exchange",
+      info: "Solana Stack Exchange is a question and answer site for Solana software users and developers.",
+    },
+    {
+      theme: "Solana Playground",
+      info: "Easily build, deploy and test Solana programs and smart contracts from a browser IDE.",
+    },
+    {
+      theme: "create-solana-dapp",
+      info: "Get up and running fast with Solana dApps, generate a project template in seconds.",
+    },
+    {
+      theme: "Anchor Docs",
+      info: "Anchor is a framework for Solana's Sealevel runtime providing several convenient developer tools for writing smart contracts.",
+    },
+    {
+      theme: "Solana Program Library",
+      info: "The Solana Program Library (SPL) is a collection of on-chain programs targeting the Sealevel parallel runtime.",
+    },
+    {
+      theme: "Program Examples",
+      info: "A list of curated examples for a wide range of use cases implemented using on-chain programs.",
+    },
+  ];
 
-  // Create and append the new elements
-  for (var i = 0; i < 7; i++) {
-    // Create the new info block
+  infoData.forEach(function (data) {
     var infoBlock = document.createElement("div");
     infoBlock.classList.add("dig_deeper_info_block");
 
-    // Create the new theme element
+    var libraryElement = document.createElement("h6");
+    libraryElement.classList.add("library");
+    libraryElement.textContent = "tool / library";
+
     var themeElement = document.createElement("div");
     themeElement.classList.add("dig_deeper_info_block_theme");
-    themeElement.textContent = " ";
+    themeElement.textContent = data.theme;
 
-    // Create the new info element
     var infoElement = document.createElement("div");
     infoElement.classList.add("dig_deeper_info_block_info");
-    infoElement.textContent = "";
-    // Append the theme and info elements to the info block
+    infoElement.textContent = data.info;
+
+    var learnElement = document.createElement("div");
+    learnElement.classList.add("dig_deeper_learn-more");
+    learnElement.textContent = "Learn more";
+
+    var btnLinkElement = document.createElement("a");
+    btnLinkElement.classList.add("btn_arrow45");
+    btnLinkElement.href = "#";
+
+    var btnImageElement = document.createElement("img");
+    btnLinkElement.classList.add("btn_arrow45_image");
+
+    btnImageElement.src = "images/dig_deeper_image/arrow45.svg";
+
+    learnElement.appendChild(btnImageElement);
+    infoBlock.appendChild(libraryElement);
     infoBlock.appendChild(themeElement);
     infoBlock.appendChild(infoElement);
+    infoBlock.appendChild(learnElement);
 
-    // Append the info block to the container
     container.appendChild(infoBlock);
-  }
+  });
 });
